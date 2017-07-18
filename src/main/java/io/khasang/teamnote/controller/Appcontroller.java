d7495a4139e5da39281706a96d9786ec1f7cb949
Azon <azon.sk@gmail.com>
2017-07-18 20:46:56 +0300

    add new rows and change old

--- src/main/java/io/khasang/teamnote/controller/Appcontroller.java
+++ src/main/java/io/khasang/teamnote/controller/Appcontroller.java
@@ -2,7 +2,10 @@ package io.khasang.teamnote.controller;
 
 public class Appcontroller {
     public static void main(String[] args) {
+        System.out.println("asd1ad");
+        System.out.println("as1asad");
         System.out.println("asdasad");
+        System.out.println("asda1");
         System.out.println("asdasad");
     }
 }
