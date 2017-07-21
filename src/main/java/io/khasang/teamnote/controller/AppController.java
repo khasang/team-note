0e58193b8ede49f091761231179b0eb5ce2180cb
Dmitriy <dgrushetskiy@gmail.com>
2017-07-21 23:21:30 +0300

    add AppController

--- src/main/java/io/khasang/teamnote/controller/AppController.java
+++ src/main/java/io/khasang/teamnote/controller/AppController.java
@@ -1,5 +1,26 @@
 package io.khasang.teamnote.controller;
 
+import io.khasang.teamnote.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
-    
+    @Value("Гектор Барбосса")
+    private final Message message;
+
+    @Autowired
+    public AppController(Message message) {
+        this.message = message;
+    }
+
+    // http://localhost:8080/
+    @RequestMapping("/")
+    public String helloPage(Model model){
+        model.addAttribute("name", message.getName());
+        return "hello";
+    }
 }
