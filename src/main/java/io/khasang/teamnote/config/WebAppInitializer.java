8e48f804dbe08ac2cbc8d728198b493d77e8700c
Dmitriy <dgrushetskiy@gmail.com>
2017-07-21 23:19:47 +0300

    add

--- src/main/java/io/khasang/teamnote/config/WebAppInitializer.java
+++ src/main/java/io/khasang/teamnote/config/WebAppInitializer.java
@@ -1,7 +1,10 @@
 package io.khasang.teamnote.config;
 
 import io.khasang.teamnote.config.application.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 /**
  * @author gothmog on 21.07.2017.
@@ -20,5 +23,16 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    /**
+     * Решает 90% проблем с кодировкой
+     */
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter =
+                new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("utf-8");
+        return new Filter[]{characterEncodingFilter};
     }
 }
