ef03e5db15e5b78f3930e391958aa755c11f2b3c
alex_jd <twoalexan@mail.ru>
2017-07-22 14:12:33 +0500

    delete my code

--- src/main/java/io/khasang/teamnote/controller/Appcontroller.java
+++ src/main/java/io/khasang/teamnote/controller/Appcontroller.java
@@ -20,19 +20,7 @@ public class Appcontroller {
         System.out.println("vfrolova_diff_33");
         System.out.println("vfrolova_diff_33");
 
-        System.out.println("aalexeen_diff_33");
-        System.out.println("aalexeen_diff_my_branch");
-
-        System.out.println("aalexeen in my branch");
-        System.out.println("aalexeen test the right way");
-
-        System.out.println("aalexeen do some changes it main repo and don't pull it");
-
-        System.out.println("aalexeen my own changes without the pull process of the test repo");
-
-        System.out.println("aalexeen make the conflict again and pull it");
-
-        System.out.println("aalexeen try to make the conflict again");
+        
 
     }
 }
