e523b8684e18ea34e30373357b4bc83211c41537
alex_jd <twoalexan@mail.ru>
2017-07-22 15:33:17 +0500

    second attempt

--- src/main/java/io/khasang/teamnote/controller/Appcontroller.java
+++ src/main/java/io/khasang/teamnote/controller/Appcontroller.java
@@ -26,7 +26,9 @@ public class Appcontroller {
         System.out.println("aalexeen do my code without the pull test repo");
         System.out.println("aalexeen, commit and push it");
 
-
+        //second attempt
+        System.out.println("aalexeen make the diff 1 (simulate the another developer's code");
+        System.out.println("aalexeen, commit and push it");
 
     }
 }
