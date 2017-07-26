6b932a7e55038ca7bfc0ff41749927192c5d30fb
Azon <azon.sk@gmail.com>
2017-07-26 21:52:26 +0300

    add spring security in DB

--- src/main/java/io/khasang/teamnote/controller/AppController.java
+++ src/main/java/io/khasang/teamnote/controller/AppController.java
@@ -4,42 +4,58 @@ import io.khasang.teamnote.model.Message;
 import io.khasang.teamnote.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
+    @Autowired
+    CatService catService;
     @Value("Jack")
     @Autowired
     private Message message;
 
-    @Autowired
-    CatService catService;
-
-
     // http://localhost:8080/
     @RequestMapping("/")
-    public String helloPage(Model model){
+    public String helloPage(Model model) {
         model.addAttribute("name", message.getName());
         return "hello";
     }
 
     @RequestMapping("/create")
-    public String statusCatTableCreation(Model model){
+    public String statusCatTableCreation(Model model) {
         model.addAttribute("create", catService.createCatTableStatus());
         return "create";
     }
 
     @RequestMapping("/admin")
-    public String getAdminPage(Model model){
+    public String getAdminPage(Model model) {
         model.addAttribute("admin", "Very Secure Page for admins!");
         return "admin";
     }
 
+    @RequestMapping("/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("user", "You are welcome!");
+        return "user";
+    }
+
     @RequestMapping("/superadmin")
-    public String getSuperAdminPage(Model model){
+    public String getSuperAdminPage(Model model) {
         model.addAttribute("superadmin", "Very Secure Page for super admins!");
         return "superadmin";
+    }
+
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
     }
 }
