4c67bf833e45dd6d4d4a4cea4ab97d85f0bf179b
Dmitriy <dgrushetskiy@gmail.com>
2017-07-28 00:46:23 +0300

    update CatDao add method insert, update, delete

--- src/main/java/io/khasang/teamnote/db/dao/CatDao.java
+++ src/main/java/io/khasang/teamnote/db/dao/CatDao.java
@@ -1,7 +1,14 @@
 package io.khasang.teamnote.db.dao;
 
+import io.khasang.teamnote.model.Cat;
+
 /**
  * @author gothmog on 28.07.2017.
  */
 public interface CatDao {
+    boolean createCat(Cat cat);
+
+    boolean updateCat(int id, String name);
+
+    void deleteCat(int id);
 }
