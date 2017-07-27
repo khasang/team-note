bd655fa9a59f4b50ac11e9d1cf31ec16e14bbd39
Dmitriy <dgrushetskiy@gmail.com>
2017-07-28 00:01:53 +0300

    update BackupTable add directory postgresql for cmd comands

--- src/main/java/io/khasang/teamnote/model/BackupTable.java
+++ src/main/java/io/khasang/teamnote/model/BackupTable.java
@@ -3,6 +3,8 @@ package io.khasang.teamnote.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
+import java.text.SimpleDateFormat;
+
 /**
  * @author gothmog on 27.07.2017.
  */
@@ -13,5 +15,16 @@ public class BackupTable {
     private String dumpAppPath = "C:\\Program Files\\PostgreSQL\\9.6\\bin\\pg_dump";
     private String dumpFolder = "C:\\Backup\\";
 
+    public String runBackupTable(){
+        String pgDump = environment.getProperty(dumpAppPath);
+        String dumpFile = environment.getProperty(dumpFolder) + getDumpFileName();
+        return null;
+    }
+
+    private String getDumpFileName(){
+        long currentTime = System.currentTimeMillis();
+        String currentStringDate = new SimpleDateFormat("yyyy_MM_dd_HH-mm").format(currentTime);
+        return "teamnote_" + currentStringDate + ".backup";
+    }
 
 }
