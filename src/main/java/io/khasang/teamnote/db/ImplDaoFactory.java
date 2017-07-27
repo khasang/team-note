edb7a9142b627e8d408c26525c27bc1733dfd9fe
Dmitriy <dgrushetskiy@gmail.com>
2017-07-28 00:39:28 +0300

    add ImplDaoFactory add CatDao

---
+++ src/main/java/io/khasang/teamnote/db/ImplDaoFactory.java
@@ -0,0 +1,7 @@
+package io.khasang.teamnote.db;
+
+/**
+ * @author gothmog on 28.07.2017.
+ */
+public class ImplDaoFactory {
+}
---
+++ src/main/java/io/khasang/teamnote/db/dao/CatDao.java
@@ -0,0 +1,7 @@
+package io.khasang.teamnote.db.dao;
+
+/**
+ * @author gothmog on 28.07.2017.
+ */
+public class CatDao {
+}
