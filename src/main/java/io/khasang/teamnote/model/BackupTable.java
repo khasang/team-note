fe05fe73a963a458248a9353ada6050fad92c8a1
Dmitriy <dgrushetskiy@gmail.com>
2017-07-27 23:01:37 +0300

    add ackupTable

---
+++ src/main/java/io/khasang/teamnote/model/BackupTable.java
@@ -0,0 +1,13 @@
+package io.khasang.teamnote.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author gothmog on 27.07.2017.
+ */
+public class BackupTable {
+
+    @Autowired
+    Environment environment;
+}
