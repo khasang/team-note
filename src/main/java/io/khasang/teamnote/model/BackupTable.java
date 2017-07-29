441895023e44941fd9c510a460d3e78126a7d016
Dmitriy <dgrushetskiy@gmail.com>
2017-07-29 17:53:54 +0300

    add BackupTable - реализация задания pgdump.exe сделать бекап какой нибудь таблицы

--- src/main/java/io/khasang/teamnote/model/BackupTable.java
+++ src/main/java/io/khasang/teamnote/model/BackupTable.java
@@ -3,7 +3,13 @@ package io.khasang.teamnote.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author gothmog on 27.07.2017.
@@ -18,7 +24,49 @@ public class BackupTable {
     public String runBackupTable() {
         String pgDump = environment.getProperty(dumpAppPath);
         String dumpFile = environment.getProperty(dumpFolder) + getDumpFileName();
-        return null;
+        //Add commands to start pg_dump
+        List<String> baseCommand = new ArrayList<>();
+        //Path to pg_dump
+        baseCommand.add(pgDump);
+        baseCommand.add("-h");
+        baseCommand.add("localhost");
+        //Port
+        baseCommand.add("-p");
+        baseCommand.add("5432");
+        //User
+        baseCommand.add("-U");
+        baseCommand.add(environment.getProperty("jdbc.postgresql.username"));
+        //Add BLOB object into dump file
+        baseCommand.add("-b");
+        baseCommand.add("-v");
+        //Path to dump file
+        baseCommand.add("-f");
+        baseCommand.add(dumpFile);
+        //Base name
+        baseCommand.add("teamnote");
+        final ProcessBuilder processBuilder = new ProcessBuilder(baseCommand);
+        //Password for PostgreSQL user
+        final Map<String, String> env = processBuilder.environment();
+        env.put("PGPASSWORD", environment.getProperty("jdbc.postgresql.password"));
+        try {
+            final Process process = processBuilder.start();
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String line = reader.readLine();
+            while (line != null) {
+                System.err.println(line);
+                line = reader.readLine();
+            }
+            reader.close();
+
+            final int dcertExitCode = process.waitFor();
+            if (dcertExitCode == 0) {
+                return "Backup complete " + dumpFile;
+            } else {
+                return "Backup failed";
+            }
+        } catch (IOException | InterruptedException e) {
+            return e.toString();
+        }
     }
 
     private String getDumpFileName() {
