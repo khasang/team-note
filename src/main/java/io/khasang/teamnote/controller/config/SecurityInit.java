5533bb9f651b2f37cbb2ea21d16aeb3aa2f3a4a1
skybenefit <frolova.victory@gmail.com>
2017-07-30 16:29:59 +0300

    add config model controller

---
+++ src/main/java/io/khasang/teamnote/controller/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package io.khasang.teamnote.controller.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
