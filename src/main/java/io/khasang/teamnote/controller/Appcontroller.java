7d511e5821e5f1c0549a59d14d2658659276fa00
skybenefit <frolova.victory@gmail.com>
2017-07-30 16:54:50 +0300

    moved files

--- src/main/java/io/khasang/teamnote/controller/Appcontroller.java
+++ src/main/java/io/khasang/teamnote/controller/Appcontroller.java
@@ -1,18 +1,61 @@
 package io.khasang.teamnote.controller;
 
-public class Appcontroller {
-    public static void main(String[] args) {
-        System.out.println("asd1ad");
-        System.out.println("as1asad");
-        System.out.println("asdasad");
-        System.out.println("a1sad");
-        System.out.println("as1ad");
-        System.out.println("asdasad");
-        System.out.println("asdasad");
-        System.out.println("asdasad");
+import io.khasang.teamnote.model.Message;
+import io.khasang.teamnote.service.CatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-        System.out.println("vfrolova");
-        System.out.println("vfrolova");
-        System.out.println("vfrolova");
+@Controller
+public class AppController {
+    @Autowired
+    CatService catService;
+    @Value("Jack")
+    @Autowired
+    private Message message;
+
+    // http://localhost:8080/
+    @RequestMapping("/")
+    public String helloPage(Model model) {
+        model.addAttribute("name", message.getName());
+        return "hello";
+    }
+
+    @RequestMapping("/create")
+    public String statusCatTableCreation(Model model) {
+        model.addAttribute("create", catService.createCatTableStatus());
+        return "create";
+    }
+
+    @RequestMapping("/admin")
+    public String getAdminPage(Model model) {
+        model.addAttribute("admin", "Very Secure Page for admins!");
+        return "admin";
+    }
+
+    @RequestMapping("/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("user", "You are welcome!");
+        return "user";
+    }
+
+    @RequestMapping("/superadmin")
+    public String getSuperAdminPage(Model model) {
+        model.addAttribute("superadmin", "Very Secure Page for super admins!");
+        return "superadmin";
+    }
+
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
     }
 }
