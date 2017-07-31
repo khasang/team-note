480ebe39cc524e75e657110de2ee859778e12292
Dmitriy <dgrushetskiy@gmail.com>
2017-08-01 00:01:21 +0300

    add SecurityInit

---
+++ src/main/java/io/khasang/teamnote/config/SecurityInit.java
@@ -0,0 +1,9 @@
+package io.khasang.teamnote.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author gothmog on 31.07.2017.
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
