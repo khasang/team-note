af4b314fd64f5463edbe0b78c74642097e2c2d5b
tdolgov <dolgov.tim@gmail.com>
2017-08-02 23:15:10 +0700

    add Sicurity

--- src/main/java/io/khasang/teamnote/controller/AppController.java
+++ src/main/java/io/khasang/teamnote/controller/AppController.java
@@ -4,9 +4,13 @@ import io.khasang.teamnote.model.Message;
 import io.khasang.teamnote.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -30,5 +34,25 @@ public class AppController {
     public String statusCatTableCreation(Model model){
         model.addAttribute("create",catService.createCatTableStatus());
         return "create";
+    }
+
+    @RequestMapping("/admin")
+    public String getAdminPage(Model model){
+        model.addAttribute("admin","Very Secure Page for admins!");
+        return "admin";
+    }
+
+    @RequestMapping("/superadmin")
+    public String getSuperAdminPage(Model model){
+        model.addAttribute("superadmin","Very Secure Page for super admins!");
+        return "superadmin";
+    }
+
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
     }
 }
