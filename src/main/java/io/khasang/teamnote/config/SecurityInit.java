7ff604488e1cb2c86f3d2c68a25bf8179c9751f2
skybenefit <frolova.victory@gmail.com>
2017-08-04 17:38:44 +0300

    delete

--- src/main/java/io/khasang/teamnote/config/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package io.khasang.teamnote.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
