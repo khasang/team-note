0582ac2643f17834f36460f3ee9542d42b566fd1
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 22:45:29 +0300

    add BasicDaoImpl

--- src/main/java/io/khasang/teamnote/dao/impl/BasicDaoImpl.java
+++ src/main/java/io/khasang/teamnote/dao/impl/BasicDaoImpl.java
@@ -1,9 +1,19 @@
 package io.khasang.teamnote.dao.impl;
 
 import io.khasang.teamnote.dao.BasicDao;
+import org.hibernate.Session;
 
 /**
  * @author gothmog on 06.08.2017.
  */
 public class BasicDaoImpl<T> implements BasicDao<T>{
+    @Override
+    public Session getCurrentSession() {
+        return null;
+    }
+
+    @Override
+    public T create(T entity) {
+        return null;
+    }
 }
