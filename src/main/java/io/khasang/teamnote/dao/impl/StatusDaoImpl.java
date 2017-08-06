2d2b50eccfd9ca4241471faaeeb8e1753e238d12
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 23:18:12 +0300

    update StatusDaoImpl

--- src/main/java/io/khasang/teamnote/dao/impl/StatusDaoImpl.java
+++ src/main/java/io/khasang/teamnote/dao/impl/StatusDaoImpl.java
@@ -1,7 +1,16 @@
 package io.khasang.teamnote.dao.impl;
 
+import io.khasang.teamnote.dao.StatusDao;
+import io.khasang.teamnote.entity.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author gothmog on 06.08.2017.
  */
-public class StatusDaoImpl extends BasicDaoImpl{
+public class StatusDaoImpl extends BasicDaoImpl<Status> implements StatusDao{
+
+    public StatusDaoImpl(Class<Status> entityClass) {
+        super(entityClass);
+    }
 }
