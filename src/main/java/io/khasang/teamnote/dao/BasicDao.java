e134bdb74122c47cea22a4b0144297d67f2aa0a2
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 22:45:15 +0300

    add BasicDao

--- src/main/java/io/khasang/teamnote/dao/BasicDao.java
+++ src/main/java/io/khasang/teamnote/dao/BasicDao.java
@@ -7,7 +7,16 @@ import org.hibernate.SessionFactory;
  * @author gothmog on 06.08.2017.
  */
 public interface BasicDao<T> {
+    /**
+     *
+     * @return current Hibernate Session
+     */
     Session getCurrentSession();
 
+    /**
+     * Create entity at database
+     * @param entity - current entity for creation
+     * @return created entity
+     */
     T create(T entity);
 }
