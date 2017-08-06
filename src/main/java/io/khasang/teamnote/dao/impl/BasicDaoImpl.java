05bafc6e0c0956fc2ce042f54bcd83510be0fb96
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 22:48:02 +0300

    update BasicDaoImpl add SessionFactory

--- src/main/java/io/khasang/teamnote/dao/impl/BasicDaoImpl.java
+++ src/main/java/io/khasang/teamnote/dao/impl/BasicDaoImpl.java
@@ -2,11 +2,16 @@ package io.khasang.teamnote.dao.impl;
 
 import io.khasang.teamnote.dao.BasicDao;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author gothmog on 06.08.2017.
  */
 public class BasicDaoImpl<T> implements BasicDao<T>{
+    @Autowired
+    SessionFactory sessionFactory;
+    
     @Override
     public Session getCurrentSession() {
         return null;
