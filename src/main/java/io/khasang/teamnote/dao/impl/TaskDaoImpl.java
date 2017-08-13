8c947e0f1f5ae2181d98e4b7a99f8858ed968c0c
skybenefit <frolova.victory@gmail.com>
2017-08-13 17:46:43 +0300

    add HW 05 06

---
+++ src/main/java/io/khasang/teamnote/dao/impl/TaskDaoImpl.java
@@ -0,0 +1,10 @@
+package io.khasang.teamnote.dao.impl;
+
+import io.khasang.teamnote.dao.TaskDao;
+import io.khasang.teamnote.entity.Task;
+
+public class TaskDaoImpl extends BasicDaoImpl<Task> implements TaskDao {
+    public TaskDaoImpl(Class<Task> entityClass) {
+        super(entityClass);
+    }
+}
