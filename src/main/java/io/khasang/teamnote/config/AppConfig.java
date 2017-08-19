d8870334539fba199c352babc359efc8ac86926f
aalexeen <alexan.sergeev@gmail.com>
2017-08-20 02:41:11 +0500

    implement dao Message

--- src/main/java/io/khasang/teamnote/config/AppConfig.java
+++ src/main/java/io/khasang/teamnote/config/AppConfig.java
@@ -1,8 +1,11 @@
 package io.khasang.teamnote.config;
 
 import io.khasang.teamnote.dao.DocumentDao;
+import io.khasang.teamnote.dao.MessageDao;
 import io.khasang.teamnote.dao.impl.DocumentDaoImpl;
+import io.khasang.teamnote.dao.impl.MessageDaoImpl;
 import io.khasang.teamnote.entity.Document;
+import io.khasang.teamnote.entity.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,6 +52,11 @@ public class AppConfig {
     @Bean
     public DocumentDao documentDao(){
         return new DocumentDaoImpl(Document.class);
+    }
+
+    @Bean
+    public MessageDao messageDao(){
+        return new MessageDaoImpl(Message.class);
     }
 
 }
---
+++ src/main/java/io/khasang/teamnote/entity/Message.java
@@ -0,0 +1,77 @@
+package io.khasang.teamnote.entity;
+
+import sun.util.resources.CalendarData;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long fromUserId;
+    private long toUserId;
+    private LocalDateTime createData;
+    private String messageText;
+    private int priority;
+    private int status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(long fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public long getToUsersIds() {
+        return toUserId;
+    }
+
+    public void setToUsersIds(long toUsersId) {
+        this.toUserId = toUserId;
+    }
+
+    public LocalDateTime getCreateData() {
+        return createData;
+    }
+
+    public void setCreateData(LocalDateTime createData) {
+        this.createData = createData;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
