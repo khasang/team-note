873117d102a7861d47a68c56237e4d613452f45a
Dmitriy <dgrushetskiy@gmail.com>
2017-08-29 00:27:21 +0300

    add ItemsDaoImpl

--- src/main/java/io/khasang/teamnote/dao/impl/ItemsDaoImpl.java
+++ src/main/java/io/khasang/teamnote/dao/impl/ItemsDaoImpl.java
@@ -1,7 +1,15 @@
 package io.khasang.teamnote.dao.impl;
 
+import io.khasang.teamnote.dao.ItemsDao;
+import io.khasang.teamnote.entity.Items;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author gothmog on 29.08.2017.
  */
-public class ItemsDaoImpl {
+public class ItemsDaoImpl extends BasicDaoImpl<Items> implements ItemsDao{
+
+    public ItemsDaoImpl(Class<Items> entityClass) {
+        super(entityClass);
+    }
 }
