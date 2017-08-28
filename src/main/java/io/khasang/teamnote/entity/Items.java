85cef78475e3b510beebd6342f43a6f93f8b638b
Dmitriy <dgrushetskiy@gmail.com>
2017-08-29 00:25:50 +0300

    add Items add ItemsDao add ItemsDaoImpl add FileBucket

---
+++ src/main/java/io/khasang/teamnote/entity/Items.java
@@ -0,0 +1,7 @@
+package io.khasang.teamnote.entity;
+
+/**
+ * @author gothmog on 28.08.2017.
+ */
+public class Items {
+}
---
+++ src/main/java/io/khasang/teamnote/model/FileBucket.java
@@ -0,0 +1,7 @@
+package io.khasang.teamnote.model;
+
+/**
+ * @author gothmog on 29.08.2017.
+ */
+public class FileBucket {
+}
