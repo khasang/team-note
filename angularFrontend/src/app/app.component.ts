4c345329ba94aa6d4c0034cd1d9346605d83a2e3
twice <dvajdi@bk.ru>
2017-10-12 14:35:52 +0300

    for my study only

--- angularFrontend/src/app/app.component.ts
+++ angularFrontend/src/app/app.component.ts
@@ -1,33 +1,13 @@
 import {Component, OnInit} from "@angular/core";
-import {Task} from "./main-board/tasks/task.model";
-import {SidebarService} from "./sidebar/sidebar.service";
-import {TestTaskBD} from "./testTaskBD";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [SidebarService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedFeature: string = 'inputTasks';
 
-  taskArray: Task[] = this.testTaskBD.taskInputArray;
-
-  constructor(private sidebarService: SidebarService,private testTaskBD:TestTaskBD) {
-
-    this.sidebarService.selectItemEmitter.subscribe(
-      (feature) => {
-        this.loadedFeature = feature;
-        if (this.loadedFeature === 'inputTasks') {
-          this.taskArray = this.testTaskBD.taskInputArray;
-        }
-        if (this.loadedFeature === 'outputTasks') {
-          this.taskArray = this.testTaskBD.taskOutputArray;
-        }
-      }
-    );
-  }
 
   ngOnInit() { }
 }
--- angularFrontend/src/app/app.module.ts
+++ angularFrontend/src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import {ServerService} from "./servers/server.service";
 
 
 
@@ -9,13 +16,19 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UserComponent,
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [ServerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
