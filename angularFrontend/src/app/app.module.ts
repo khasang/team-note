aeaf3ed93407a1c13dfbf4596947cbce294a6473
Twice <dvajdi@bk.ru>
2017-10-21 19:25:12 +0300

    WIP:work with routes: page-not-found

--- angularFrontend/src/app/app.module.ts
+++ angularFrontend/src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {RouterModule, Routes} from '@angular/router'
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ServersComponent } from './servers/servers.component';
-import { UsersComponent } from './users/users.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { UserComponent } from './users/user/user.component';
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
+import {ServersComponent} from "./servers/servers.component";
+import {UsersComponent} from "./users/users.component";
+import {EditServerComponent} from "./servers/edit-server/edit-server.component";
+import {ServerComponent} from "./servers/server/server.component";
+import {UserComponent} from "./users/user/user.component";
 import {ServerService} from "./servers/server.service";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const appRouts:Routes = [
-  {path:"servers",component:ServersComponent, children: [
-    {path:":id",component:ServerComponent},
-    {path:":id/edit",component:EditServerComponent}
-  ]},
-  {path:"users",component:UsersComponent,children: [
-    {path:":id/:name",component:UserComponent}
-  ]},
-  {path:"",component:HomeComponent}
-  ];
+const appRouts: Routes = [
+  {path: "servers", component: ServersComponent, children: [
+    {path: ":id", component: ServerComponent},
+    {path: ":id/edit", component: EditServerComponent}]},
+  {path: "users", component: UsersComponent, children: [
+    {path: ":id/:name", component: UserComponent}]  },
+  {path: "", component: HomeComponent},
+  {path:"not-found", component:PageNotFoundComponent},
+  {path:"**",redirectTo:"/not-found"}
+];
 
 
 @NgModule({
@@ -33,6 +34,7 @@ const appRouts:Routes = [
     EditServerComponent,
     ServerComponent,
     UserComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,4 +44,5 @@ const appRouts:Routes = [
   providers: [ServerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
