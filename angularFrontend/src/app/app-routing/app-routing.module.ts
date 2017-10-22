d8614fda1cd6e05a668762b6c51ff268524d0a39
Twice <dvajdi@bk.ru>
2017-10-22 11:44:44 +0300

    end training

--- angularFrontend/src/app/app-routing/app-routing.module.ts
+++ angularFrontend/src/app/app-routing/app-routing.module.ts
@@ -6,7 +6,6 @@ import {EditServerComponent} from "../servers/edit-server/edit-server.component"
 import {UsersComponent} from "../users/users.component";
 import {UserComponent} from "../users/user/user.component";
 import {HomeComponent} from "../home/home.component";
-import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
 import {AuthGuardService} from "../auth-guard/auth-guard.service";
 import {ErrorPageComponent} from "../error-page/error-page.component";
 
@@ -14,14 +13,14 @@ const appRouts: Routes = [
   {
     path: "servers", component: ServersComponent, children: [
     {path: ":id", component: ServerComponent},
-    {path: ":id/edit", canActivate:[AuthGuardService],component: EditServerComponent}]
+    {path: ":id/edit", canActivate: [AuthGuardService], component: EditServerComponent}]
   },
   {
     path: "users", component: UsersComponent, children: [
     {path: ":id/:name", component: UserComponent}]
   },
   {path: "", component: HomeComponent},
-  {path: "not-found", component: ErrorPageComponent,data:{errorMessage:"Старница не найдена"}},
+  {path: "not-found", component: ErrorPageComponent, data: {errorMessage: "Старница не найдена"}},
   {path: "**", redirectTo: "/not-found"}
 ];
 
