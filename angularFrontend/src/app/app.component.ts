3eeac6762a159aa2828e1178825d27f823027a99
Twice <dvajdi@bk.ru>
2017-10-24 22:09:40 +0300

    WIP:AddForms

--- angularFrontend/src/app/app.component.ts
+++ angularFrontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,12 @@ import {Component, OnInit} from "@angular/core";
   providers: []
 })
 export class AppComponent implements OnInit {
+  suggestUserName(){
+    const suggestedName = 'Superuser';
+  }
 
-
+  onSubmit(form:NgForm){
+    console.log(form.value);
+  }
   ngOnInit() { }
 }
