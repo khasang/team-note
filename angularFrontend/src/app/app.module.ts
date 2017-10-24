5abde614e27c3618987d4fae541883f6e96cf5c0
Twice <dvajdi@bk.ru>
2017-10-24 21:44:02 +0300

    WIP:AddForms

--- angularFrontend/src/app/app.module.ts
+++ angularFrontend/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
