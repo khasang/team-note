9fc157e466ca2cb0725f3b673d2123ff055b719c
Twice <dvajdi@bk.ru>
2017-11-11 13:59:26 +0300

    WIP:AddForms

--- angularFrontend/src/app/app.module.ts
+++ angularFrontend/src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
+import {RouterModule, Routes} from "@angular/router";
+import {TasksComponent} from "./tasks/tasks.component";
+import { UsersComponent } from './users/users.component';
 
-
-
+const routes: Routes = [
+  {path: "tasks", component: TasksComponent},
+  {path: "users", component: UsersComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    TasksComponent,
+    UsersComponent,
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
