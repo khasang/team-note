502ede4bbbbd90f8831962745389fbff28bfcbb7
twice <dvajdi@bk.ru>
2017-12-06 13:10:45 +0300

    WIP: editServices

--- angularFrontend/src/app/data-services/data-service/data-service.service.ts
+++ angularFrontend/src/app/data-services/data-service/data-service.service.ts
@@ -2,23 +2,26 @@ import {Injectable} from "@angular/core";
 import {Http,Response} from "@angular/http";
 import {TaskService} from "../../main-board/tasks/task.service";
 import {Task} from "../../main-board/tasks/task.model";
+import {Subject} from "rxjs/Subject";
 
 @Injectable()
 export class DataServiceService {
 
-  constructor(private http: Http, private taskService: TaskService) {
+  dataLoadSubject = new Subject();
+
+  constructor(private http: Http) {
   }
 
-  saveTaskToDB() {
-    return this.http.put("https://team-note-firebase.firebaseio.com/tasks.json", this.taskService.getTasks());
+  saveTaskToDB(tasks:Task[]) {
+    console.log(tasks);
+    return this.http.put("https://team-note-firebase.firebaseio.com/tasks", tasks);
   }
 
   getTaskFromDB() {
     this.http.get("https://team-note-firebase.firebaseio.com/tasks.json")
       .subscribe(
         (response: Response) => {
-          const tasks: Task[] = response.json();
-          this.taskService.setTasksFromDB(tasks);
+          this.dataLoadSubject.next(response.json())
         }
       );
   }
