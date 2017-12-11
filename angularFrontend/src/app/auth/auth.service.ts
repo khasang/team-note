af5ec2007d4ed41b6d05952e289fe7a97ee3f215
Twice <dvajdi@bk.ru>
2017-12-11 21:33:11 +0300

    add using tokens

--- angularFrontend/src/app/auth/auth.service.ts
+++ angularFrontend/src/app/auth/auth.service.ts
@@ -1,20 +1,35 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
-export class AuthService{
-  signupUser(email:string,password:string){
-    firebase.auth().createUserWithEmailAndPassword(email,password)
+export class AuthService {
+  token: string;
+
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
         error => console.log(error)
       )
   }
 
-  signinUser(email:string,password:string){
-    firebase.auth().signInWithEmailAndPassword(email,password)
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-        response=>console.log(response)
+        response => {
+          firebase.auth().currentUser.getToken()
+            .then(
+              (token:string) => this.token=token
+            )
+        }
       )
       .catch(
-        error=>console.log(error)
+        error => console.log(error)
       )
+  }
+
+  getToken(){
+     firebase.auth().currentUser.getToken()
+       .then(
+         (token: string) => this.token=token
+       );
+    return this.token;
   }
 }
--- angularFrontend/src/app/data-services/data-service/data-service.service.ts
+++ angularFrontend/src/app/data-services/data-service/data-service.service.ts
@@ -1,19 +1,25 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import {Task} from "../../app-entities/task.entity";
+import {AuthService} from "../../auth/auth.service";
 
 @Injectable()
 export class DataServiceService {
 
-  constructor(private http: Http) {
+  constructor(private http: Http,
+              private authService: AuthService) {
   }
 
   saveTaskToDB(tasks:Task[]) {
-    return this.http.put("https://team-note-firebase.firebaseio.com/tasks.json", tasks);
+    const token  = this.authService.getToken();
+    console.log("token = "+token);
+    return this.http.put("https://team-note-firebase.firebaseio.com/tasks.json?auth="+token, tasks);
   }
 
   getTaskFromDB() {
-     return this.http.get("https://team-note-firebase.firebaseio.com/tasks.json");
+     const token  = this.authService.getToken();
+     console.log("token = "+token);
+     return this.http.get("https://team-note-firebase.firebaseio.com/tasks.json?auth="+token);
   }
 
 }
